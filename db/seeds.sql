e09784c890f6cb0eaa9bf0b346252af5709b4a14
Cesar Martinez <cesaguma@hotmail.com>
2020-11-15 22:12:52 -0500

    Making package.json file. Intalling npm packages (express, express-handlebars, mysql). Creating schema.sql and seeds.sql files to genrate the Database. Creating connection.js file.

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (burger_name) VALUES ("Mcchicken");
+INSERT INTO burgers (burger_name) VALUES ("McBouble");
+INSERT INTO burgers (burger_name) VALUES ("McBacon");
+INSERT INTO burgers (burger_name) VALUES ("Cheese");
+INSERT INTO burgers (burger_name) VALUES ("Quarter");
+INSERT INTO burgers (burger_name) VALUES ("Veggi");
